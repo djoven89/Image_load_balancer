fab1f7dde597a870e766b965263140a186b3269f
djoven89 <smrdaniel30@gmail.com>
2017-04-25 23:15:36 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+set -eo pipefail
+shopt -s nullglob
 
-for i in {1..5}; do
-  x=`env | grep WEB$i | cut -d "=" -f2`
-  echo $x
-  grep -o 172.20.0.2$i /etc/nginx/sites-available/default | xargs -I '{}' sed -i "s/{}/$x/" /etc/nginx/sites-available/default
-
+for i in {1..5}
+ do
+  if [ $WEB$i ] 
+   then
+      echo $WEB$i >> /archivo.txt
+#     x=`env | grep WEB$i | cut -d "=" -f2`
+#     echo $x
+#     grep -o 172.20.0.2$i /etc/nginx/sites-available/default | xargs -I '{}' sed -i "s/{}/$x/" /etc/nginx/sites-available/default
+   else
+      echo "No existe." >> /archivo.txt
+  fi  
 done
+
+exec "$@"
