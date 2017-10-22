bffbdf2a7b554e161ffbed1c047504464fba7773
djoven89 <smrdaniel30@gmail.com>
2017-10-22 23:03:59 +0200

    Optimización
    Se han modificado variables y se ha optimizado el código

--- config.sh
+++ config.sh
@@ -1,44 +1,41 @@
 #!/bin/bash -x
 
-ARCHIVO="/etc/nginx/sites-available/default"
+NGINXCONF="/etc/nginx/sites-available/default"
 
 # Compruebo existe el archivo de configuración de Nginx
-if [ ! -f $ARCHIVO ]
+if [ ! -f $NGINXCONF ]
   then
      echo 'No se ha creado el fichero todavía...' >> /dev/stderr
      exit 1
 fi
 
 # Variable para quedarse con las variables de entorno en orden
-X=0
+X=1
 
 # Variable para filtrar por la IP por defecto del fichero de configuración
 Y=0
 
 # Guardo en un fichero temporal el número de servidores web
-env | grep -i web > /tmp/ip.txt
+env | grep WEB > /tmp/ips
 
 # Me quedo con el número de servidores WEB
-NUMSRV=`cat /tmp/ip.txt | wc -l`
+NUMSRV=`cat /tmp/ips | wc -l`
 
 if [ $NUMSRV > 0 ]
   then
      for (( i=1; i<=$NUMSRV; i++ ))
-	do
-	   # Cuenta atrás para quedarse con el servidor correcto
-	   X=`expr $X + 1`
-					
+	do			
 	   # Guarda la IP del servidor web 
-	   IPLOAD=$(cat /tmp/ip.txt | cut -d "=" -f2 | head -`echo $X` | tail -1)
+	   IPSRV=$(cat /tmp/ips | cut -d "=" -f2 | head -`echo $X` | tail -1)
 					
 	   # Compruebo de que exista la dirección IP por defecto del archivo de configuración
 	   # en caso de coincidir, la modifico por el servidor que toque y descomento la línea
-	   if [ `grep -o 172.17.0.2$Y $ARCHIVO` ]
+	   if [ `grep -o 172.17.0.2$Y $NGINXCONF` ]
 	     then
-		  grep -o 172.17.0.2$Y $ARCHIVO | xargs -I {} sed -i "s/{}/$IPLOAD/" $ARCHIVO
-		  sed -i "s/#  server $IPLOAD/server $IPLOAD/" $ARCHIVO
-		  Y=`expr $Y + 1`
+		  grep -o 172.17.0.2$Y $NGINXCONF | xargs -I {} sed -i "s/{}/$IPSRV/; s/^#server $IPSRV/  server $IPSRV/" $NGINXCONF
+		  (( Y++ ))
 	   fi
+	  (( X++ ))
 	done
   else
      echo 'No se han especificado servidores web.' >> /dev/stderr
